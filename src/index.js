23ef6407dad12f6dd4ebeef493cce0352d0d4962
Daniel Seeley <Daniel.h.seeley@gmail.com>
2018-11-17 15:18:37 -0500

    Added test API Key

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import App from "./components/App";
 // Replace `YOUR_API_KEY` here with the API key you received when
 // signing up for SimpleWebRTC
 // --------------------------------------------------------------------
-const API_KEY = 'YOUR_API_KEY';
+const API_KEY = 'a94404a2a1595d2222ae78f2';
 // ====================================================================
 
 
